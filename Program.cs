1d8959cf0d45c04bb8b37669c6a42037e5734403
Priya-Stake <priya@stake.rent>
2025-03-30 20:52:25 +0530

    swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -15,6 +19,13 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())  // Swagger only in development mode
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseCors("AllowGitHub");
 
 // Configure the HTTP request pipeline.
@@ -27,5 +38,3 @@ app.UseHttpsRedirection();
 
 
 app.Run();
-
-
